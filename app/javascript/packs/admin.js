31a3022b2bfab953d65707cbec1f34d2e935a679
Fritjof Höst <fritjof.host@digitalroute.com>
2021-07-20 15:53:15 +0200

    further separation of concerns

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -1,6 +1,8 @@
 import {Chart, LineElement, LineController, CategoryScale, LinearScale, PointElement} from 'chart.js';
 Chart.register(LineElement, LineController, CategoryScale, LinearScale, PointElement);
 
+import Sortable from 'sortablejs'
+
 document.addEventListener('turbolinks:load', () => {
   
   var ctx = document.getElementById('page-views');
@@ -37,5 +39,39 @@ document.addEventListener('turbolinks:load', () => {
         }]
       }
     });
+  }
+
+  document.addEventListener('click', (event) => {
+    let element = event.target.closest('.paragraph-content')
+    if (!element) return;
+
+    element.classList.add('d-none')
+    element.nextElementSibling.classList.remove('d-none')
+  })
+
+  document.addEventListener('click', (event) => {
+    if (!event.target.matches('.cancel')) return;
+    event.preventDefault()
+
+    let element = event.target.closest('.paragraph-form')
+
+    element.classList.add('d-none')
+    element.previousElementSibling.classList.remove('d-none')
+  })
+
+  document.addEventListener('click', (event) => {
+    if (!event.target.matches('.close')) return;
+    let element = event.target.closest('.paragraph-form')
+
+    element.classList.add('d-none')
+    element.previousElementSibling.classList.remove('d-none')
+  
+    var html = "<div class='trix-content'>" + element.querySelector("input[name='element[content]']").value + "</div>"
+    element.previousElementSibling.innerHTML = html
+  })
+
+  if (document.getElementById('elements')) {
+    let element = document.getElementById('elements')
+    Sortable.create(elements, { animation: 150 })
   }
 })
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,12 +9,10 @@ import mrujs, { CableCar } from "mrujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "controllers"
+import '../stylesheets/application';
 
-mrujs.start({
-  plugins: [
-    new CableCar(CableReady)
-  ]
-})
+mrujs.start()
 Turbolinks.start()
 ActiveStorage.start()
 
@@ -22,46 +20,3 @@ const Trix = require("trix")
 require("@rails/actiontext")
 
 Trix.config.blockAttributes.heading1.tagName = "h3";
-
-import Sortable from 'sortablejs'
-import "controllers"
-
-import '../stylesheets/application';
-
-document.addEventListener('turbolinks:load', () => {
-
-  document.addEventListener('click', (event) => {
-    let element = event.target.closest('.paragraph-content')
-    if (!element) return;
-
-    element.classList.add('d-none')
-    element.nextElementSibling.classList.remove('d-none')
-  })
-
-  document.addEventListener('click', (event) => {
-    if (!event.target.matches('.cancel')) return;
-    event.preventDefault()
-
-    let element = event.target.closest('.paragraph-form')
-
-    element.classList.add('d-none')
-    element.previousElementSibling.classList.remove('d-none')
-  })
-
-  document.addEventListener('click', (event) => {
-    if (!event.target.matches('.close')) return;
-    let element = event.target.closest('.paragraph-form')
-
-    element.classList.add('d-none')
-    element.previousElementSibling.classList.remove('d-none')
-  
-    var html = "<div class='trix-content'>" + element.querySelector("input[name='element[content]']").value + "</div>"
-    element.previousElementSibling.innerHTML = html
-  })
-
-  if (document.getElementById('elements')) {
-    let element = document.getElementById('elements')
-    Sortable.create(elements, { animation: 150 })
-  }
-})
-

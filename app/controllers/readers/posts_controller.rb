525b9102c725df46766f9679ad39f0b0839fe7c7
Fritjof Höst <fritjof.host@digitalroute.com>
2021-07-21 12:09:46 +0200

    related tags

--- app/controllers/readers/posts_controller.rb
+++ app/controllers/readers/posts_controller.rb
@@ -5,5 +5,14 @@ module Readers
       @tags = @post.tags
       impressionist(@post)
     end
+
+    def tagged
+      if params[:tag].present?
+        @tag = ActsAsTaggableOn::Tag.where(id: params[:tag]).first
+        @posts = Post.published.tagged_with(@tag)
+      else
+        @posts = Post.published.all
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   resources :elements
   devise_for :authors, controllers: { registrations: "registrations" }
   get 'home/index'
-  get '/about', to: 'readers/about#index'
-  
   root to: "readers/home#index"
-  get '/blog/:id' => 'readers/posts#show', as: :blog_post
+  
+  scope module: 'readers' do
+    get '/tagged' => "posts#tagged", as: :tagged
+    get '/about' => 'about#index'
+    get '/blog/:id' => 'posts#show', as: :blog_post
+  end
+  
   
   scope module: 'authors' do
     get 'stats' => 'stats#index'

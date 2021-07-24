4f487b7476b22b33f4d802d0dce1421daa4038af
Fritjof Höst <fritjof.host@digitalroute.com>
2021-07-24 14:27:01 +0200

    fixed broken loop, added reading time and some other improvements

--- app/controllers/readers/home_controller.rb
+++ app/controllers/readers/home_controller.rb
@@ -1,7 +1,20 @@
 module Readers
   class HomeController < ReadersController
+    helper_method :reading_time
+
     def index
       @posts = Post.published.most_recently_published
+    end
+
+    def reading_time(post)
+      words_per_minute = 150
+      @content = ""
+      post.elements.each do |element|
+        if element.element_type == "paragraph"
+          @content = @content + element.content.body.to_plain_text
+        end
+      end
+      (@content.scan(/\w+/).length / words_per_minute).to_i
     end
   end
 end
--- app/controllers/readers/posts_controller.rb
+++ app/controllers/readers/posts_controller.rb
@@ -1,9 +1,23 @@
 module Readers
   class PostsController < ReadersController
+    helper_method :reading_time
+
     def show
       @post = Post.published.friendly.find(params[:id])
       @tags = @post.tags
+      
       impressionist(@post)
+    end
+
+    def reading_time(post)
+      words_per_minute = 150
+      @content = ""
+      post.elements.each do |element|
+        if element.element_type == "paragraph"
+          @content = @content + element.content.body.to_plain_text
+        end
+      end
+      (@content.scan(/\w+/).length / words_per_minute).to_i
     end
 
     def tagged
